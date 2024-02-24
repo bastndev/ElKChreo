baf6c2e41b801c794061eefa233a54f74ab172a4
bastndev <gohit24@gmail.com>
2024-02-24 17:19:27 -0500

    :arrow_up: UPDATE `Controlle folder`

--- lib/pages/controllers/auth_service.dart
+++ lib/pages/controllers/auth_service.dart
@@ -1,8 +1,37 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-  static final FirebaseAuth _auth = FirebaseAuth.instance;
+  static final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
+  static String verifyId = "";
+  // to sent and otp to user
+  static Future sentOtp({
+    required String phone,
+    required Function errorStep,
+    required Function nextStep,
+  }) async {
+    await _firebaseAuth
+        .verifyPhoneNumber(
+      timeout: const Duration(seconds: 30),
+      phoneNumber: "+51$phone",
+      verificationCompleted: (phoneAuthCredential) async {
+        return;
+      },
+      verificationFailed: (error) async {
+        return;
+      },
+      codeSent: (verificationId, forceResendingToken) async {
+        verifyId = verificationId;
+        nextStep();
+      },
+      codeAutoRetrievalTimeout: (verificationId) async {
+        return;
+      },
+    )
+        .onError((error, stackTrace) {
+      errorStep();
+    });
+  }
 
-  //to send and otp user
+  // verify the otp code and login
 }
