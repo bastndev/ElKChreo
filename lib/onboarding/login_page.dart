b5a1b474464203639ff7f4e7b29c5f6cc2e41ed3
bastndev <gohit24@gmail.com>
2024-02-27 10:55:57 -0500

    :building_construction: `Add button`

--- lib/onboarding/login_page.dart
+++ lib/onboarding/login_page.dart
@@ -1,16 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:practice/onboarding/login_items.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final controller = LoginItems();
+  final pageController = PageController();
+
+  bool isLastPage = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                itemCount: controller.items.length,
+                controller: pageController,
+                itemBuilder: (context, index) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(controller.items[index].image),
+                      const SizedBox(height: 20),
+                      Text(controller.items[index].title),
+                      Text(controller.items[index].description),
+                    ],
+                  );
+                },
+              ),
+            ),
+            getStarted(),
+          ],
+        ),
       ),
-      body: const Center(
-        child: Text('Login Page'),
+    );
+  }
+
+  Widget getStarted() {
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        color: Colors.pink,
+      ),
+      width: MediaQuery.of(context).size.width * .9,
+      height: 55,
+      child: TextButton(
+        onPressed: () {},
+        child: const Text(
+          "Get Started",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
