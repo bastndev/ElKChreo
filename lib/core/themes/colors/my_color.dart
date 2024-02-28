eba788d227b7a93e3cfdd6f64041d6798573336e
bastndev <gohit24@gmail.com>
2024-02-28 15:45:33 -0500

    :arrow_up: UPDATE `Add my_color`

--- lib/core/themes/colors/my_color.dart
+++ lib/core/themes/colors/my_color.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MyColor {
+  // Buttons Colors
   static const Color mainButton = Color.fromARGB(255, 32, 32, 32);
   static const Color secondButton = Color.fromARGB(255, 221, 222, 221);
 
+  // Card Color
   static const Color card = Color.fromARGB(255, 255, 255, 255);
 }
