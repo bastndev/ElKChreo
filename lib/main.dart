5ebf01758705882aa159e240f46dc9b95555c3f0
bastndev <gohit24@gmail.com>
2024-02-28 11:22:51 -0500

    :arrow_up: UPDATE `Refactory code`

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:practice/pages/home/home_page.dart';
-import 'package:practice/pages/auth/login/login_page.dart';
+import 'package:practice/pages/auth/login/login_card.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // home: const LoginPage(),
-      home: onboarding ? const HomePage() : const LoginPage(),
+      home: onboarding ? const HomePage() : const LoginCard(),
     );
   }
 }
---
+++ lib/pages/auth/login/login_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:practice/pages/auth/login/widget/button_home.dart';
+
+class LoginCard extends StatefulWidget {
+  const LoginCard({super.key});
+
+  @override
+  State<LoginCard> createState() => _LoginCardState();
+}
+
+class _LoginCardState extends State<LoginCard> {
+  TextEditingController userNameController = TextEditingController();
+
+  final pageController = PageController();
+
+  bool isLastPage = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            TextField(
+              controller: userNameController,
+              decoration: InputDecoration(
+                hintText: "Name",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            ButtonHome(userNameController: userNameController),
+            const SizedBox(height: 40),
+          ],
+        ),
+      ),
+    );
+  }
+}
